538641e3271460fe43f98e3dcab08f981888ab44
YasharVahedein <44170133+YasharVahedein@users.noreply.github.com>
2018-10-29 18:56:28 -0400

    Update PTTcalc.m

--- Code V1.01/PTTcalc.m
+++ Code V1.01/PTTcalc.m
@@ -1,4 +1,31 @@
 function PTTcalc(PWV2,AT,HMESH,NCELL,PTTfunc,AU0,AREA0)
+%**************
+% MIT License
+% 
+% Copyright (c) 2018 <Yashar Seyed Vahedein, Alexander Liberson>
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a
+% copy of this software and associated documentation files (the
+% "Software"), to deal in the Software without restriction, including
+% without limitation the rights to use, copy, modify, merge, publish,
+% distribute, sublicense, and/or sell copies of the Software, and to permit
+% persons to whom the Software is furnished to do so, subject to the
+% following conditions:
+% 
+% The above copyright notice and this permission notice shall be included
+% in all copies or substantial portions of the Software.
+%
+% The user is recommended to reference the first released publication based on this code:
+% 
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+% OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+% MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+% NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+% DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+% OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+% USE OR OTHER DEALINGS IN THE SOFTWARE.
+%**************
 
 STARTtime=PTTfunc(2);
 FINISHtime=PTTfunc(3);
