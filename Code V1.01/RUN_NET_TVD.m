2b9a22b1b4110674cc3751336e62760302a49a72
Yashar Seyed Vahedein <ys5978@ad.rit.edu>
2018-11-28 10:56:58 -0500

    Update RUN_NET_TVD.m

--- Code V1.01/RUN_NET_TVD.m
+++ Code V1.01/RUN_NET_TVD.m
@@ -120,7 +120,7 @@ end
 %=============================================
 %% Time loop Starts
 %=============================================
-AT(1)=0;  t=0;          %Initialize time and its array
+    AT(1)=0;  t=0;   counterSTA=0; counterEND=0;       %Initialize time and PTT values
 
 for itau=1:100000000    %Run Until reahcing TIME
     
@@ -144,6 +144,16 @@ for itau=1:100000000    %Run Until reahcing TIME
     % Time advancing
     %=============================================
     t=t+dtau;                                           % time
+            %for PTT calculation
+            if PTTfunc(1)==1
+                if counterSTA==0 && t>PTTfunc(7)*TT
+                    PTTfunc(2)=itau-300;
+                    counterSTA=counterSTA+1;
+                elseif  counterEND==0 && t>(PTTfunc(7)+1)*TT
+                    PTTfunc(3)=itau-300;
+                    counterEND=counterEND+1;
+                end
+            end
     AT(itau)=t;                                         %ARRAY of Time
     if(t>TIME) || imag(t)~=0
         AT=AT(1:itau-1);                                %Accumulate time array and break when it ends
